4db896a3a58cce10ff3ef16bb3f82f5438b8fbdd
Gerardo Belot <gbelot2003@hotmail.com>
2017-09-11 14:47:48 -0600

    implementando routers en modulo separado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
+import { routes } from "./app.routes";
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {ImageService} from './services/image/image.service';
+import { ImageService } from './services/image/image.service';
+
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar.component';
@@ -10,14 +12,6 @@ import { ImageComponent } from './gallery/images-list/image.component';
 import { ImageDetailComponent } from './gallery/image-detail/image-detail.component';
 import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
-import {Routes, RouterModule} from '@angular/router';
-
-const appRoutes: Routes = [
-  {path: '', redirectTo: '/gallery', pathMatch: 'full'},
-  {path: 'gallery', component: GalleryComponent},
-  {path: 'contact', component: ContactComponent},
-  {path: 'about', component: AboutComponent}
-];
 
 @NgModule({
   declarations: [
@@ -31,7 +25,7 @@ const appRoutes: Routes = [
     AboutComponent
   ],
   imports: [
-    RouterModule.forRoot(appRoutes),
+    routes,
     BrowserModule
   ],
   providers: [ImageService],
