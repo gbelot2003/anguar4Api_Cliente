6d5bb270aff0024e8365ebab4c6966a9bee750a8
Gerardo Belot <gbelot2003@hotmail.com>
2017-09-11 12:10:07 -0600

    Usando ngIf para mostrar componente opcional

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {ImageService} from './services/image/image.service';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { ImagesListComponent } from './gallery/images-list/images-list.component';
 import { ImageComponent } from './gallery/images-list/image.component';
+import { ImageDetailComponent } from './gallery/image-detail/image-detail.component';
 
 @NgModule({
   declarations: [
@@ -13,12 +15,13 @@ import { ImageComponent } from './gallery/images-list/image.component';
     NavbarComponent,
     GalleryComponent,
     ImagesListComponent,
-    ImageComponent
+    ImageComponent,
+    ImageDetailComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ImageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/image/image.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Image } from '../../models/image';
+
+@Injectable()
+export class ImageService {
+  images: Image[] = [
+    new Image('1', 'Primer Imagen', "Descripcion primera imagen", '/assets/imagenes/thum/1gal.jpg', '/assets/imagenes/1gal.jpg'),
+    new Image('2', 'Segunda Imagen', "Descripcion primera imagen", '/assets/imagenes/thum/2gal.jpg', '/assets/imagenes/2gal.jpg'),
+    new Image('3', 'Tercera Imagen', "Descripcion primera imagen", '/assets/imagenes/thum/3gal.jpg', '/assets/imagenes/3gal.jpg'),
+    new Image('4', 'Cuarta Imagen', "Descripcion primera imagen", '/assets/imagenes/thum/4gal.jpg', '/assets/imagenes/4gal.jpg'),
+    new Image('5', 'Quinta Imagen', "Descripcion primera imagen", '/assets/imagenes/thum/5gal.jpg', '/assets/imagenes/5gal.jpg'),
+    new Image('6', 'Sexta Imagen', "Descripcion primera imagen", '/assets/imagenes/thum/6gal.jpg', '/assets/imagenes/6gal.jpg'),
+    new Image('7', 'Septima Imagen', "Descripcion primera imagen", '/assets/imagenes/thum/7gal.jpg', '/assets/imagenes/7gal.jpg'),
+    new Image('1', 'Octava Imagen', "Descripcion primera imagen", '/assets/imagenes/thum/8gal.jpg', '/assets/imagenes/8gal.jpg'),
+  ];
+  constructor() { }
+  
+  getImages(){
+    return this.images;
+  }
+
+}
